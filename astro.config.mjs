a83347ad0909491772b8892355df7518fa128703
praavar0302-boop <praavar0302@gmail.com>
2026-07-18 23:08:27 +0530

    Optimize mobile layout and implement instant page transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,10 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://caniflythere.com',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'tap',
+  },
   integrations: [
     sitemap({
       filter: (page) => !page.includes('/404') && !page.includes('/500'),
